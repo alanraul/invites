038c59ebcbe1c57e86cc6f3c11c276a90fc08014
alanraul <raulumb@gmail.com>
2018-08-15 14:35:13 -0500

    Mejora en la alineación y se evita romper palabras

--- src/pillow/index.py
+++ src/pillow/index.py
@@ -11,6 +11,25 @@ from PIL import ImageFont
 from PIL import ImageDraw
 
 
+def add_spaces(text, number_char, align):
+    """Añade espacios al texto para centrarlo.
+
+    Args:
+        text (str): Texto para dibujar.
+        number_char (int): Número de caracteres por párrafo.
+        align (str): Tipo de alineación.
+
+    Returns:
+        str: Texto con espacios a los lados.
+    """
+
+    white_spaces = len(text)
+
+    if align == "center":
+        white_spaces = len(text) + (number_char - len(text))
+
+    return text.center(white_spaces)
+
 def get_file(uri, name):
     """Obtiene un archivo por medio de una URI.
 
@@ -36,27 +55,42 @@ def set_font_color(color):
 
     return tuple(int(color.lstrip('#')[i:i+2], 16) for i in (0, 2 ,4))
 
-def multiline_text(text, number_char):
+def multiline_text(text, number_char, align):
     """Añade saltos de linea al texto.
 
     Args:
         text (str): Texto para dibujar.
         number_char (int): Número de caracteres por párrafo.
+        align (str): Tipo de alineación.
 
     Returns:
         str: Texto con saltos de linea.
     """
 
-    i = 0
     multiline_text = ""
+    paragraph = ""
+    counter = 0
 
-    while i <= len(text):
-        multiline_text += text[i:number_char] + "\n"
+    last_word = len(text.split(" "))
+    words = text.split(" ")
 
-        i += number_char
-        number_char += number_char
+    for i in range(0, len(words)):
+        len_chars = len(words[i] + " ") + counter
+
+        if len_chars < number_char:
+            paragraph += words[i] + " "
+            counter = len(paragraph)
+
+        if len_chars > number_char:
+            multiline_text += add_spaces(paragraph.rstrip(), number_char, align) + "\n"
+            paragraph = ""
+            counter = 0
+
+        if last_word == i+1:
+            multiline_text += add_spaces(words[i], number_char, align) + "\n"
 
     return multiline_text
+
 
 if __name__ == '__main__':
     invite = json.loads(sys.argv[1])
@@ -73,7 +107,7 @@ if __name__ == '__main__':
         font = get_file(os.environ["FONTS_BUCKET"], text["font"])
         font = ImageFont.truetype(font, text["size"])
 
-        text["text"] = multiline_text(text["text"], text["number_char"])
+        text["text"] = multiline_text(text["text"], text["number_char"], text["align"])
 
         draw.multiline_text(coordinates, text["text"], fill=color, font=font,
                         anchor=None, spacing=text["spacing"], align=text["align"])
--- src/priv/repo/seeds.exs
+++ src/priv/repo/seeds.exs
@@ -16,41 +16,42 @@ alias Invites.Contexts.TextsManager
 {:ok, invite} = InvitesManager.create(%{template: "01.png"})
 
 TextsManager.create(%{
-  coordinates: "140,90",
+  coordinates: "440,240",
   font: "Gotham-Book.otf",
-  number_char: 22,
+  number_char: 20,
   color: "#11a1e4",
   tag: "event",
-  size: 18,
-  invite_id: invite.id
+  size: 38,
+  invite_id: invite.id,
+  align: "left"
 })
 
 TextsManager.create(%{
-  coordinates: "30,120",
+  coordinates: "30,380",
   font: "Gotham-Book.otf",
-  number_char: 22,
+  number_char: 13,
   color: "#2d2fa4",
   tag: "celebrated",
-  size: 60,
+  size: 90,
   invite_id: invite.id
 })
 
 TextsManager.create(%{
-  coordinates: "70,260",
+  coordinates: "180,560",
   font: "Gotham-Book.otf",
-  number_char: 12,
+  number_char: 28,
   color: "#e3717f",
   tag: "place",
-  size: 18,
+  size: 32,
   invite_id: invite.id
 })
 
 TextsManager.create(%{
-  coordinates: "50,200",
+  coordinates: "180,760",
   font: "Gotham-Book.otf",
-  number_char: 17,
+  number_char: 30,
   color: "#e3717f",
   tag: "date",
-  size: 18,
+  size: 28,
   invite_id: invite.id
 })
